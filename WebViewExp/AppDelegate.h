25a718a5e432c811f69feccc5fa21871ab777669
LinBq <bq_lin@hotmail.com>
2016-11-02 17:46:20 +0800

    Initial Commit

---
+++ WebViewExp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WebViewExp
+//
+//  Created by LinBq on 16/11/2.
+//  Copyright © 2016年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WebViewExp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WebViewExp
+//
+//  Created by LinBq on 16/11/2.
+//  Copyright © 2016年 POLYV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
