ed32b4def6975e15fecca2a904cb720917d83500
bqlin <bqlins@163.com>
2018-03-21 15:27:09 +0800

    更新项目，更改项目结构

--- README.md
+++ README.md
@@ -1,7 +1,3 @@
 # WebView-Experimentation
 
 UIWebView 与 WKWebView 测试。
-
-[蒲公英内测页面](https://www.pgyer.com/9unm)
-
-![下载二维码](https://www.pgyer.com/app/qrcode/9unm)
--- WebViewExp/WebViewController/UIWebViewExp.m
+++ WebViewExp/WebViewController/UIWebViewExp.m
@@ -21,9 +21,18 @@
     
     _webView.mediaPlaybackRequiresUserAction = NO;
     _webView.allowsInlineMediaPlayback = YES;
+	
+	self.automaticallyAdjustsScrollViewInsets = NO;
+	UISearchBar *searchBar = [[UISearchBar alloc] init];
+	searchBar.searchBarStyle = UISearchBarStyleMinimal;
+	searchBar.text = @"https://www.baidu.com/";
+	
+	self.navigationItem.titleView = searchBar;
     
     self.textField.delegate = self;
 }
+
+
 
 - (IBAction)go:(id)sender {
     NSLog(@"%s", __FUNCTION__);
@@ -41,14 +50,5 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
