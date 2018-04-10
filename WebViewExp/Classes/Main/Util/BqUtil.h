a359f8e056d8691f5ac21c6701345e9c0baf2144
bqlin <bqlins@163.com>
2018-04-10 10:30:06 +0800

    优化编译

--- WebViewExp/Classes/Main/Util/BqUtil.h
+++ WebViewExp/Classes/Main/Util/BqUtil.h
@@ -6,16 +6,17 @@
 //  Copyright © 2018年 Bq. All rights reserved.
 //
 
-#if __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0
+#define XCode9 __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_11_0
+
+#if XCode9
 // available for iOS 11
+#define BQ_AVAILABLE(v) @available(iOS v, *)
 
 #else
 // can not use iOS 11 API
+#define BQ_AVAILABLE(v) ([UIDevice currentDevice].systemVersion.floatValue > (v))
 
 #endif
-
-#define BQ_AVAILABLE(v) @available(iOS v, *)
-//#define BQ_AVAILABLE(v) ([UIDevice currentDevice].systemVersion.floatValue > (v))
 
 #define IS_IPAD UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad
 
